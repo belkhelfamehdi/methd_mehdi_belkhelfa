d003a6958119ccd700a65ee0bd7b20d0299b739a
Mehdi Belkhelfa <midou8687@gmail.com>
2025-06-24 09:33:23 +0200

    first commit

---
+++ src/main/java/com/ynov/mehdi/MehdiApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.mehdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MehdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MehdiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynov/mehdi/MehdiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.mehdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MehdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
